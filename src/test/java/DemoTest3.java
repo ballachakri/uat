39460c5c77cea7991e66e7f77a08d6ed2e2729a7
chakravarthy <ballachakri@yahoo.com>
2019-10-12 18:26:51 +0100

    pipeline demo

--- src/test/java/DemoTest3.java
+++ src/test/java/DemoTest3.java
@@ -7,13 +7,13 @@ public class DemoTest3 {
 
     @Before
     public void demoTest3Before() {
-        System.out.println("This a before method in demoTest3Before method...................");
+        System.out.println("This a before method in demoTest3  Before method...................");
     }
 
     @Test
     public void demoTest3Test() {
-        System.out.println("Demo Test 1 test method executed.......................... ");
-        System.out.println("SANITY TEST Successfully Passed");
+        System.out.println("Demo Test 3 test method executed.......................... ");
+        System.out.println("User Acceptance Test Successfully Passed");
     }
 
     @After
